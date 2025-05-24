b4e7355ab3a1cd61debd4c509314bc7f2e0ca85a
Myannovation <myannovation@gmail.com>
2025-05-25 00:20:39 +0700

    Final code change

---
+++ app/page.js
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      background: '#f0f0f0'
+    }}>
+      <h1 style={{
+        fontSize: '4rem',
+        fontWeight: 'bold',
+        color: '#333'
+      }}>
+        Hello, World
+      </h1>
+    </div>
+  );
+}
